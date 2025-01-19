c3fabbb541b4f989da8cde11b53175a5003994e7
kalebeccs <kalebe.correa.santos@gmail.com>
2025-01-19 23:39:03 +0000

    Add docstring to main function in run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from app.wine_app import create_app
 
-if __name__ == "__main__":
+def main():
+    """
+    Entry point for the Rosa Negra application.
+    
+    This function creates an instance of the WineApp and starts the main event loop.
+    """
     app = create_app()
     app.mainloop()
+
+if __name__ == "__main__":
+    main()
